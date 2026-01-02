905485b66f7f832bba440f93859ca2b9d1f5a6bf
Synapse Dev <dev@synapse.com>
2026-01-02 20:09:20 +0000

    Remove orphaned ZoomImageViewLib widget classes
    - Deleted OnScaleAndMoveGestureListener interface
    - Deleted ScaleAndMoveDetector class
    - Deleted AnimCompat object
    - Deleted SpringInterpolator class
    - Deleted WindowManagerUtil object
    
    All classes were unused/orphaned with no references in codebase.
    Build passes with no compilation errors.

--- app/src/main/java/com/synapse/social/studioasinc/widget/ZoomImageViewLib/gestures/OnScaleAndMoveGestureListener.kt
+++
@@ -1,9 +0,0 @@
-package com.synapse.social.studioasinc.widget.ZoomImageViewLib.gestures
-
-import android.view.ScaleGestureDetector
-
-interface OnScaleAndMoveGestureListener {
-    fun onScaleAndMove(scaleGestureDetector: ScaleGestureDetector, currentScale: Float, moveDistanceFromX: Float, moveDistanceFromY: Float)
-    fun onScaleBegin(scaleGestureDetector: ScaleGestureDetector)
-    fun onScaleEnd(scaleGestureDetector: ScaleGestureDetector, moveDistanceFromX: Float, moveDistanceFromY: Float)
-}
